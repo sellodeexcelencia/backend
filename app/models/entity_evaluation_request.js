ddb4bd2b5d7be3e41045c2e026bbd03104355c69
dfjimenezt <daniel.jimenez@tee.com.co>
2018-06-08 11:37:32 -0500

    request_status alert

--- app/models/entity_evaluation_request.js
+++ app/models/entity_evaluation_request.js
@@ -138,7 +138,7 @@ var Evaluation_request = function () {
 			JOIN questiontopic qt ON qt.id = q.id_topic
 			JOIN institution i ON i.id = s.id_institution
 			JOIN request_status r_s ON e_r.id_request_status
-			WHERE e_r.alert = 1 AND e_r.id_request_status IN (${status.join(',')})
+			WHERE r_s.alert = 1 AND e_r.id_request_status IN (${status.join(',')})
 			${end_time ? 'AND DATE(e_r.end_time) = \''+end_time+'\' ':''}
 			${alert_time ? 'AND DATE(e_r.alert_time) = \''+alert_time+'\' ':''}`
 		return this.customQuery(q)
